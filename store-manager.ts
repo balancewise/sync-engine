04ce13fabd0001ef5f1819acf168bf497f48ccbe
PJ <ipriyanshujain@gmail.com>
2025-02-15 21:41:48 +0530

    initial model design

---
+++ store-manager.ts
@@ -0,0 +1,47 @@
+import "fake-indexeddb/auto";
+import { ModelRegistry } from "./model-registry";
+import { ObjectStore, FullObjectStore, PartialObjectStore } from "./object-store";
+
+class StoreManager {
+    private stores = new Map<string, ObjectStore>();
+
+    async initialize() {
+        const models = ModelRegistry.getModels();
+        for (const model of models) {
+            let store: ObjectStore;
+
+            switch (model.loadStrategy) {
+                case 'partial':
+                    store = new PartialObjectStore(ModelRegistry.getModelMetadata(model));
+                    break;
+                default:
+                    store = new FullObjectStore(ModelRegistry.getModelMetadata(model));
+            }
+            await store.initialize();
+
+            this.stores.set(model.name, store);
+            await new Promise(resolve => setTimeout(resolve, 0)); // Allow store to initialize
+            this.createDatabase(store);
+        }
+    }
+
+    private createDatabase(store: ObjectStore) {
+        const dbName = store.getDatabaseName();
+        const request = indexedDB.open(dbName, 1);
+
+        request.onupgradeneeded = (event) => {
+            const db = request.result;
+            store.createStore(db);
+        };
+
+        request.onsuccess = () => {
+            console.log(`Database ${dbName} initialized`);
+        };
+    }
+
+    getStore(modelName: string): ObjectStore | undefined {
+        return this.stores.get(modelName);
+    }
+}
+
+export { StoreManager };
